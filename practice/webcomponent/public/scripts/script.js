1afe936e9f3bff2a2b2a6ef26aca3fa069d8a991
saurabhdiwanji <saurabh.diwanji@gmail.com>
2024-06-14 21:25:11 +0200

    removed console.log

--- practice/webcomponent/public/scripts/script.js
+++ practice/webcomponent/public/scripts/script.js
@@ -5,15 +5,12 @@ dropZone.addEventListener("dragover", dragOverHandler);
 const myCard = document.querySelectorAll("my-card");
 
 for (i = 0; i < myCard.length; i++) {
-  console.log(myCard[i]); 
   myCard[i].addEventListener("dragstart", dragStartHandler);
 }
 
 function dragStartHandler(ev) {
   ev.dataTransfer.setData("text", ev.target.id);
-  console.log(`dragging ${ev.target.id}`);
   ev.dataTransfer.dropEffect = "copy"; 
-  console.log(`event = ${ev}`)  
 }
 
 function dragOverHandler(ev) {
@@ -22,9 +19,7 @@ function dragOverHandler(ev) {
 
 function dropHandler(ev) {
   ev.preventDefault();
-  console.log("drop event = ", ev)
   ev.dataTransfer.dropEffect = "copy";
   const data = ev.dataTransfer.getData("text");
-  console.log("from dropEvent data = ", data)
   ev.target.appendChild(document.getElementById(data).cloneNode(true));
 }
